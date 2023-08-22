9cfe65efbe46d55c31338ba95f2a061d9956dc25
Elliot Akande <elbo.lynch@gmail.com>
2023-08-22 10:32:18 +0100

    Remove complete state from todoItem

--- src/todoItem.js
+++ src/todoItem.js
@@ -3,22 +3,16 @@ export default function todoItem(title, description, dueDate, priority) {
     let _description = description;
     let _dueDate = dueDate;
     let _priority = priority;
-    let _complete = false;
 
     const getTitle = () => _title;
     const getDescription = () => _description;
     const getDueDate = () => _dueDate;
     const getPriority = () => _priority;
-    const isComplete = () => _complete;
-
-    const toggleComplete = () => _complete = !_complete;
 
     return {
         getTitle,
         getDescription,
         getDueDate,
         getPriority,
-        isComplete,
-        toggleComplete,
     }
 }
