8ef0aa4dc0d39d7db9e13096e4ee235ddad807b1
Elliot Akande <elbo.lynch@gmail.com>
2023-08-31 10:47:58 +0100

    Add new task button to header

--- src/displayController.js
+++ src/displayController.js
@@ -1,11 +1,14 @@
 import sidebarController from "./sidebarController";
 import mainController from "./mainController";
+import taskModal from "./taskModal";
 
 const displayController = (() => {
     const render = () => {
         _renderHeader();
         sidebarController.render();
         mainController.render();
+
+        _registerEventListeners();
     }
 
     const _renderHeader = () => {
@@ -13,9 +16,26 @@ const displayController = (() => {
         const header = document.createElement('header');
         contentDiv.appendChild(header);
 
+        // Header text
         const headerText = document.createElement('h1');
         headerText.textContent = 'ToDo List';
         header.appendChild(headerText);
+
+        // Create Item Button
+        const button = document.createElement('button');
+        button.textContent = 'Add Task';
+        button.classList.add('add-task-header');
+        header.appendChild(button);
+    }
+
+    const _addListItem = () => {
+        const modal = taskModal();
+        modal.render();
+    } 
+
+    const _registerEventListeners = () => {
+        const addListItemButton = document.querySelector('.add-task-header');
+        addListItemButton.addEventListener('click', _addListItem);
     }
 
     return {
